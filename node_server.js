ff3aaaaac60bee7467cee70cbf86bf826d9fc699
Cheng Chi (@joychester) <joychester@gmail.com>
2020-07-09 23:19:57 -0700

    Update node_server.js

--- node_server.js
+++ node_server.js
@@ -13,25 +13,30 @@ const credentialsFunc = function (id) {
     return credentials;
 };
 
-const server = http.createServer( (req, res) => {
+const server = http.createServer((req, res) => {
     let status = 200;
 
     // hawk function
     console.log('calling hawk algo...');
-        (async () => {
-            try {
-                const { credentials, artifacts } = await Hawk.server.authenticate(req, credentialsFunc);
-                console.log(`Hello ${credentials.user} ${artifacts.ext}`);
-            } catch (err) {
-                console.log(`[ERROR]: Hawk auth error => ${err.message}`);
-                status = 401;
-            }
-            console.log('server start responding...');
-            res.writeHead(status, {'Content-Type': 'application/json'});
-            res.end(`Hawk auth headers status code: ${status}`);
-            console.log('server response ended...')
-        })();
-    
+    (async () => {
+        try {
+            const {
+                credentials,
+                artifacts
+            } = await Hawk.server.authenticate(req, credentialsFunc);
+            console.log(`Hello ${credentials.user} ${artifacts.ext}`);
+        } catch (err) {
+            console.log(`[ERROR]: Hawk auth error => ${err.message}`);
+            status = 401;
+        }
+        console.log('server start responding...');
+        res.writeHead(status, {
+            'Content-Type': 'application/json'
+        });
+        res.end(`Hawk auth headers status code: ${status}`);
+        console.log('server response ended...')
+    })();
+
 });
 
 server.listen(port, host, () => {
